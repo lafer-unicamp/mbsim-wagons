f9e564287fc1456c9ef00b0fbed32e032cbe8374
leo-bart <lbaruffas@gmail.com>
2021-08-27 13:37:00 -0300

    Testes com movimentos rígidos
    Elemento desenvolvido até testes com elementos rígidos. Próximo passo: implementar montagem das matrizes globais.

--- src/Elementos_flexiveis/nachbagauer.py
+++ src/Elementos_flexiveis/nachbagauer.py
@@ -14,9 +14,10 @@ Created on Thu May 13 14:49:04 2021
 @author: Leonardo Bartalini Baruffaldi
 """
 
-from numpy.matlib import matrix, block, eye, zeros
-from numpy.linalg import norm, inv    
+import numpy as np
 
+from numpy.matlib import matrix, block, eye, zeros
+from numpy.linalg import norm, inv, det
 
 class node(object):
     """
@@ -50,7 +51,7 @@ class node(object):
         return self.q + self.q0
     
     
-
+#################################3
 class material(object):
     '''
     material
@@ -92,34 +93,34 @@ class linearElasticMaterial(material):
         self.rho = rho
         
         # Lamé constants
-        mu = self.E / (1+self.nu) * 0.5
-        lam = self.nu * self.E / ( (1+self.nu)*(1-2*self.nu) )
+        self.mu = self.E / (1+self.nu) * 0.5
+        self.lam = self.nu * self.E / ( (1+self.nu)*(1-2*self.nu) )
         
         D = eye(6)*0
         
         # following Lai, Introduction to Continuum Mechanics, 2010 notation
-        C11 = lam + 2*mu
+        C11 = self.lam + 2*self.mu
         D[0,0] = C11
         D[1,1] = C11
         D[2,2] = C11
         
-        D[0,1] = lam
-        D[1,2] = lam
-        D[2,3] = lam
-        D[1,0] = lam
-        D[2,1] = lam
-        D[3,2] = lam
+        D[0,1] = self.lam
+        D[1,2] = self.lam
+        D[2,3] = self.lam
+        D[1,0] = self.lam
+        D[2,1] = self.lam
+        D[3,2] = self.lam
         
-        D[3,3] = 2*mu
-        D[4,4] = 2*mu
-        D[5,5] = 2*mu
+        D[3,3] = 2*self.mu
+        D[4,4] = 2*self.mu
+        D[5,5] = 2*self.mu
         
         self.constitutiveMatrix = D
         
     
     
     
-  
+########################################
 class flexibleBody(object):
     '''
     Flexible body class
@@ -144,12 +145,42 @@ class flexibleBody(object):
         
         self.name = name
         self.material = material
+        self.elementList = []
         
         
-class element(object):
+    def assembleFEProblem(self):
+        self.assembleMassMatrix()
+        
+    def assembleMassMatrix(self):
+        
+        ned = 4             # element degrees of freedom
+        nel = len(self.elementList)
+        
+        
+        
+        
+        
+        
     
-    def __init__(self, parentBody):
-        self.parentBody = parentBody
+    def addElement(self, element):
+        for el in element:
+            el.parentBody = self
+        self.elementList.extend(element)
+        
+        
+        
+        
+         
+
+    
+########################################
+class element(object):
+    '''
+    Generic class for finite elements
+    '''
+    def __init__(self):
+        self.parentBody = None
+    
 
 
 
@@ -159,24 +190,44 @@ class elementLinear(element):
     Planar finite element with linear interpolation
     """
      
-    def __init__(self, node1, node2, height, parentBody):
-        element.__init__(self,parentBody)
+    def __init__(self, node1, node2, height):
         self.length = norm(node1.qtotal[0:2] - node2.qtotal[0:2])
         self.height = height
         self.nodes = [node1,node2]
         
-        L = self.length
-        h = self.height
-        self.mass = self.parentBody.material.rho * L * h
-        self.massMatrix = self.parentBody.material.rho * L * matrix([
-            [  h/3,     0,         0,         0,   h/6,     0,         0,         0],
-            [    0,   h/3,         0,         0,     0,   h/6,         0,         0],
-            [    0,     0,   h**3/36,         0,     0,     0,   h**3/72,         0],
-            [    0,     0,         0,   h**3/36,     0,     0,         0,   h**3/72],
-            [  h/6,     0,         0,         0,   h/3,     0,         0,         0],
-            [    0,   h/6,         0,         0,     0,   h/3,         0,         0],
-            [    0,     0,   h**3/72,         0,     0,     0,   h**3/36,         0],
-            [    0,     0,         0,   h**3/72,     0,     0,         0,   h**3/36]])
+        # L = self.length
+        # h = self.height
+        # self.mass = self.parentBody.material.rho * L * h
+        # self.massMatrix = self.parentBody.material.rho * L * matrix([
+        #     [  h/3,     0,         0,         0,   h/6,     0,         0,         0],
+        #     [    0,   h/3,         0,         0,     0,   h/6,         0,         0],
+        #     [    0,     0,   h**3/36,         0,     0,     0,   h**3/72,         0],
+        #     [    0,     0,         0,   h**3/36,     0,     0,         0,   h**3/72],
+        #     [  h/6,     0,         0,         0,   h/3,     0,         0,         0],
+        #     [    0,   h/6,         0,         0,     0,   h/3,         0,         0],
+        #     [    0,     0,   h**3/72,         0,     0,     0,   h**3/36,         0],
+        #     [    0,     0,         0,   h**3/72,     0,     0,         0,   h**3/36]])
+    
+    @property
+    def qtotal(self):
+        """nodal displacement relative to global frame"""
+        myq = np.empty((0,0))
+        for node in self.nodes:
+            myq = np.append(myq,node.qtotal)
+            
+        return matrix(myq)
+    
+    @property
+    def q(self):
+        myq = np.empty((0,0))
+        for node in self.nodes:
+            myq = np.append(myq,node.q)
+            
+        return matrix(myq)
+    
+    @property
+    def q0(self):
+        return block([self.nodes[0].q0, self.nodes[1].q0])
         
         
     def interpolatePosition(self,xi_, eta_):
@@ -286,30 +337,51 @@ class elementLinear(element):
         return 0.5 * (F.T * F - eye(2))
     
     def strainTensorDerivative(self,xi_,eta_):
+        '''
+        Gets the strain tensor derivative at a certain point of the element
+        (given by xi_ and eta_).
+
+        Parameters
+        ----------
+        xi_ : DOUBLE
+            LONGITUDINAL POSITION [-1,1].
+        eta_ : DOUBLE
+            TRANVERSAL POSITION [-1,1].
+
+        Returns
+        -------
+        deps_dq : NUMPY.NDARRAY
+            3 rd order tensor of the strain tensor derivative.
+            deps_dq[:,:,n] can be used to access the n-th slice of the tensor
+
+        '''
         invJ0 = self.inverseInitialJacobian(xi_,eta_)
         
-        q = block([self.nodes[0].qtotal,self.nodes[1].qtotal]).T
+        q = block([self.nodes[0].qtotal,self.nodes[1].qtotal])
         
         U11 = (self.shapeFunctionDerivative(0,0,xi_,eta_).T*
                self.shapeFunctionDerivative(0,0,xi_,eta_) + 
                self.shapeFunctionDerivative(1,0,xi_,eta_).T*
-               self.shapeFunctionDerivative(1,0,xi_,eta_))*q
+               self.shapeFunctionDerivative(1,0,xi_,eta_))
         U12 = (self.shapeFunctionDerivative(0,0,xi_,eta_).T*
                self.shapeFunctionDerivative(0,1,xi_,eta_) + 
                self.shapeFunctionDerivative(1,0,xi_,eta_).T*
-               self.shapeFunctionDerivative(1,1,xi_,eta_))*q
-        U21 = (self.shapeFunctionDerivative(0,1,xi_,eta_).T*
-               self.shapeFunctionDerivative(0,0,xi_,eta_) + 
-               self.shapeFunctionDerivative(1,1,xi_,eta_).T*
-               self.shapeFunctionDerivative(1,0,xi_,eta_))*q
+               self.shapeFunctionDerivative(1,1,xi_,eta_))
+        U21 = U12.T
         U22 = (self.shapeFunctionDerivative(0,1,xi_,eta_).T*
                self.shapeFunctionDerivative(0,1,xi_,eta_) + 
                self.shapeFunctionDerivative(1,1,xi_,eta_).T*
-               self.shapeFunctionDerivative(1,1,xi_,eta_))*q
+               self.shapeFunctionDerivative(1,1,xi_,eta_))
                      
-        U = block([[U11,U12],[U21,U22]])
-        print(U)
-        return invJ0.T * U * invJ0
+        U = block([[q*U11,q*U12],[q*U21,q*U22]])
+        
+        deps_dq = np.zeros((2,2,8))
+        
+        for m in range(8):
+            deps_dq[:,:,m] = invJ0.T * U[:,(m,m+8)] * invJ0
+        
+       
+        return deps_dq
     
     
     def getMassMatrix(self):
@@ -338,8 +410,60 @@ class elementLinear(element):
         """        
         return self.parentBody.material.rho * M * self.length * self.height / 4
     
+    def stressTensorByPosition(self,xi_,eta_):
+        return self.stressTensor(self.strainTensor(xi_, eta_))
     
-    def getNodalForces(self):
+    def stressTensor(self,strainTensor):
+        '''
+        Calcultes the stress tensor given a strain tensor and 
+        supposing the material is linear elastic
+
+        Parameters
+        ----------
+        strainTensor : MATRIX
+            2nd order strain tensor.
+
+        Returns
+        -------
+        2nd order stress tensor
+        
+        TODO (2021.08.23):
+        -------
+        This method should be placed inside the MATERIAL class, because it
+        depends on the constitutive model. I'll wait, however, until the model
+        is full 3D
+        
+        Reference:
+        Lai, W. Michael, David Rubin, e Erhard Krempl. 
+        Introduction to continuum mechanics. 
+        4th ed. Amsterdam ; Boston: Butterworth-Heinemann/Elsevier, 2010. pp 208
+
+        '''
+        
+        T = zeros([2,2])
+        delta = eye(2)
+        
+        # gets Lamè constants from material
+        mu = self.parentBody.material.mu
+        lam = self.parentBody.material.lam
+        
+        # this is the trace of the strain tensor
+        e = strainTensor.diagonal().sum()
+        
+        # regular stress tensor
+        for i in range(2):
+            for j in range(2):
+                T[i,j] = lam*e*delta[i,j] + 2*mu*strainTensor[i,j]
+                
+                
+        # TODO locking-free stress tensor
+                
+                
+        return T
+        
+        
+    
+    def getNodalElasticForces(self):
         # Gauss integration points
         gauss = []
         gauss.append(-0.7745967)
@@ -355,11 +479,34 @@ class elementLinear(element):
         Qe = zeros([8,1])
         
         # Gaussian quadrature
-        for i in range(3):
-            for j in range(3):
-                print('Not implemented')
+        
+        for p in range(3):
+            for b in range(3):
+                'inner quadrature'
+                detJ0 = det(self.initialJacobian(gauss[p], gauss[b]))
+                deps_dq = self.strainTensorDerivative(gauss[p], gauss[b])
+                T = self.stressTensor(self.strainTensor(gauss[p], gauss[b]))
+                for i in range(2):
+                    for j in range(2):
+                        'tensor product'
+                        for m in range(8):
+                            Qe[m,0] = Qe[m,0] + deps_dq[i,j,m]*T[i,j] * detJ0 * w[p] * w[b]
+                            
         
         
+        return Qe * self.length * self.height / 4
+    
+    
+    def getWeightNodalForces(self,grav):
+        L = self.length
+        H = self.height
+        return L * H * 0.25 * grav * matrix([
+            [2, 0, 0, 0, 2, 0, 0, 0],
+            [0, 2, 0, 0, 0, 2, 0, 0]])*eye(8)*self.parentBody.material.rho
+
+                            
+        
+       
                 
            
 
@@ -368,14 +515,23 @@ TEST PROGRAM
 '''
 
 
-steel = linearElasticMaterial('Steel',210e3,0.3,7.85e-6)
+steel = linearElasticMaterial('Steel',210e3,0.0,7.85e-6)
 body = flexibleBody('Bar',steel)
 
 
-n1 = node(0,0,0,1)
-n2 = node(10,0,0,1)
-x = elementLinear(n1, n2, 1,body)
-# displace node 2
-n2.q = matrix([[0.01,0,0,0]])
+n1 = node(0.0,0.0,0.0,1.0)
+n2 = node(10.0,0.0,0.0,1.0)
+n3 = node(20.0,0.0,0.0,1.0)
+x = elementLinear(n1, n2, 1)
+y = elementLinear(n2, n3, 1)
 
-#x.strainTensorDerivative(0,0)
+body.addElement([x,y])
+body.assembleMassMatrix()
+# displace node 2
+
+
+
+    
+            
+            
+            
